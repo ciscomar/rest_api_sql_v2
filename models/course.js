18b2bee479964b5e0347288b860e7e2f8c449348
ciscomar <47339599+ciscomar@users.noreply.github.com>
2019-06-10 14:04:06 -0600

    models

---
+++ models/course.js
@@ -0,0 +1,48 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+
+    const Course = sequelize.define("Course", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        userId: {
+            type: DataTypes.INTEGER
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    msg: "A Course Title is required"
+                }
+            }
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    msg: "A Course Description is required"
+                }
+            }
+        },
+        estimatedTime: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        materialsNeeded: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    },
+    );
+
+    Course.associate = (models) => {
+        Course.belongsTo(models.User);
+    };
+
+    return Course;
+};
---
+++ models/user.js
@@ -0,0 +1,58 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+
+    const User = sequelize.define("User", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    msg: "User's First name is required"
+                }
+            }
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    msg: "User's Last name is required"
+                }
+            }
+        },
+        emailAddress: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    msg: "User's Email is required"
+                },
+                isEmail: {
+                    msg: "Please use a valid email address"
+                }
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    msg: "A Password is required"
+                }
+            }
+        },
+    },
+    );
+
+    User.associate = (models) => {
+        User.hasMany(models.Course);
+    };
+
+    return User;
+};
